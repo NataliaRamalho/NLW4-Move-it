d58b761b8563274fccffb675b7a5fdb9921ca553
Natália Ramalho <NataliaRamalho@users.noreply.github.com>
2021-02-24 13:56:26 -0300

    Terceiro dia
    Criação do componente ChallengeBox,  contexto de desafios e regras de negócio.

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,46 +1,79 @@
-import { useState, useEffect } from 'react'
-import styles from '../styles/components/Countdown.module.css'
+import { useState, useEffect, useContext } from "react";
+import { ChallengeContext } from "../contexts/ChallengeContext";
+import styles from "../styles/components/Countdown.module.css";
 
-export function Countdown(){
-    const [time, setTime] = useState(1 * 60)
-    const[active, setActive] = useState(false)
+let countdownTimeout: NodeJS.Timeout;
 
-    const minutes =  Math.floor(time / 60);
-    const seconds =  time % 60;
+export function Countdown() {
+  const {startNewChallenge} = useContext(ChallengeContext);
+  const [time, setTime] = useState(0.05 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
-    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
-    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
 
-    function startCountdown(){
-        setActive(true);
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
+
+  function startCountdown() {
+    setIsActive(true);
+  }
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(0.05 * 60);
+  }
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
     }
-    useEffect(() => {
-        if(active && time > 0){
-            setTimeout(()=>{
-                setTime(time-1);
-            },1000)
-        }
-    }, [active, time]);
+  }, [isActive, time]);
 
-    return(
-     <div>
-           <div className={styles.countdownContainer}>
-         <div>
-             <span> {minuteLeft}</span>
-             <span> {minuteRight}</span>
-         </div>
-         <span>:</span>
-         <div>
-             <span> {secondLeft}</span>
-             <span> {secondRight}</span>
-         </div>
-       </div>
-       <button type="button" 
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span> {minuteLeft}</span>
+          <span> {minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span> {secondLeft}</span>
+          <span> {secondRight}</span>
+        </div>
+      </div>
+      {hasFinished ? (
+        <button disabled type="button" className={styles.countdownButton}>
+          Ciclo encerrado
+        </button>
+      ) : (
+        <>
+          {isActive ? (
+            <button
+              type="button"
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo
+            </button>
+          ) : (
+            <button
+              type="button"
               className={styles.countdownButton}
               onClick={startCountdown}
-              >
-           Iniciar um ciclo
-       </button>
-     </div>
-    )
+            >
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/global.css'
 
+import {ChallengeContext, ChallengesProvider} from '../contexts/ChallengeContext'
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+    
+  )
 }
 
 export default MyApp
